670378116eab312fd9047da0e1c3eec5ded46db6
JeanKeitwo16 <jeancarlonunessilva@gmail.com>
2025-06-17 19:35:23 -0300

    Editar atualizado

--- categoria/editar_ok.php
+++ categoria/editar_ok.php
@@ -1,16 +1,31 @@
 <?php
-    include_once "../class/categoria.class.php";
-    include_once "../class/categoriaDAO.class.php";
+session_start();
 
-    $obj = new categoria();
-    $obj->setNome($_POST["nome"]);
-    $obj->setId($_POST["id"]);
+include_once "../class/categoria.class.php";
+include_once "../class/categoriaDAO.class.php";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST["id"];
+    $nome = $_POST["nome"];
+
+    $obj = new Categoria();
+    $obj->setId($id);
+    $obj->setNome($nome);
 
     $objDAO = new categoriaDAO();
     $retorno = $objDAO->editar($obj);
-    if($retorno)
-        echo "Editado com sucesso";
-    else
-        echo "Erro ao editar";
 
-?>
+    if ($retorno) {
+        $_SESSION['message'] = "Categoria editada com sucesso!";
+        $_SESSION['message_type'] = "success";
+        header("Location: ../index.php");
+    } else {
+        $_SESSION['message'] = "Erro ao editar categoria.";
+        $_SESSION['message_type'] = "error";
+        header("Location: editar.php?id=" . $id);
+    }
+    exit();
+} else {
+    header("Location: ../index.php");
+    exit();
+}
